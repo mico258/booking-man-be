303a29f4025b49e0c4bf892a03f223505eb490f2
mico <mccuandar@gmail.com>
2021-03-15 20:02:46 +0700

    make simple log

--- lib/logger/logger.go
+++ lib/logger/logger.go
@@ -2,44 +2,10 @@ package logger
 
 import (
 	"bytes"
-	"io"
 	"os"
 
 	"github.com/sirupsen/logrus"
 )
-
-type Fields logrus.Fields
-
-type Logger interface {
-	WithFields(fields Fields) *logrus.Entry
-	WithField(key string, val interface{}) *logrus.Entry
-	WithError(err error) *logrus.Entry
-	Debugf(format string, args ...interface{})
-	Infof(format string, args ...interface{})
-	Printf(format string, args ...interface{})
-	Warnf(format string, args ...interface{})
-	Warningf(format string, args ...interface{})
-	Errorf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})
-	Panicf(format string, args ...interface{})
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Print(args ...interface{})
-	Warn(args ...interface{})
-	Warning(args ...interface{})
-	Error(args ...interface{})
-	Fatal(args ...interface{})
-	Panic(args ...interface{})
-	Debugln(args ...interface{})
-	Infoln(args ...interface{})
-	Println(args ...interface{})
-	Warnln(args ...interface{})
-	Warningln(args ...interface{})
-	Errorln(args ...interface{})
-	Fatalln(args ...interface{})
-	Panicln(args ...interface{})
-	SetOut(out io.Writer)
-}
 
 // InitializeLog setup configuration and return logrus object
 func init() {
